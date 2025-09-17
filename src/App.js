d42e0ef26a2a6574830a21338f3e6f73c9cc198a
tejasrastogileet <tejasrastogi789@gmail.com>
2025-09-17 14:48:31 +0530

    first commit - counter app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
